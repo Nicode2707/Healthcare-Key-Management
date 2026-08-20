09ce58aed2d565f05c3b905e35b53ad597622655
Niraj Kumar <nirajkr2707@gmail.com>
2026-08-20 15:38:12 +0530

    Add key management backend

---
+++ backend/key-management/src/main/java/com/healthcare/keymanagement/KeyManagementApplication.java
@@ -0,0 +1,15 @@
+package com.healthcare.keymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyManagementApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(KeyManagementApplication.class, args);
+
+	}
+
+}
---
+++ backend/key-management/src/test/java/com/healthcare/keymanagement/KeyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthcare.keymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
